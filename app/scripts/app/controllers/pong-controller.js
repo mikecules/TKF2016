5e5f36f8904f3e1e52ec2752af5729ac71c3cc06
Michael Moncada <michael.moncada@utoronto.ca>
2016-05-01 18:27:46 -0400

    * Added animating bar chart.

--- app/scripts/app/controllers/pong-controller.js
+++ app/scripts/app/controllers/pong-controller.js
@@ -14,100 +14,102 @@
         };
 
         _ctrl.players = [new VisPlayer('Duke'), new VisPlayer('Arnold')];
-      });
-
-      function VisPlayer(name) {
-
-        var CHARACTERISTICS = {
-          SKILL: 0,
-          ENERGY: 1,
-          CONFIDENCE: 2,
-          AGGRESSION: 3,
-          LUCK: 4
-        };
-
-        var _visPlayer = this,
-            _name = name || 'Player',
-            _score = 0,
-            _characteristics = [
-              {attr: 'Skill', value: 0.1, successWeight: 0.2, winInc: 0.05, loseInc: 0.02},
-              {attr: 'Energy', value: 0.5, successWeight: 0.2, winInc: 0.02, loseInc: 0.01},
-              {attr: 'Confidence', value: 0.5, successWeight: 0.2, winInc: 0.1, loseInc: -0.05},
-              {attr: 'Aggression', value: 0.5, successWeight: 0.2, winInc: 0, loseInc: 0.1},
-              {attr: 'Luck', value: 0.5, successWeight: 0.2, winInc: -0.05, loseInc: 0.05}
-            ],
-            _characteristicsCopy;
 
 
-        function _updateCharacteristics(hasWon) {
-          var incPropertyName = hasWon === true ? 'winInc' : 'loseInc';
+        function VisPlayer(name) {
 
-          for (var i = 0; i < _characteristics.length; i++) {
-            var attr = _characteristics[i];
-            attr.value += attr[incPropertyName];
+          var CHARACTERISTICS = {
+            SKILL: 0,
+            ENERGY: 1,
+            CONFIDENCE: 2,
+            AGGRESSION: 3,
+            LUCK: 4
+          };
+
+          var _visPlayer = this,
+              _name = name || 'Player',
+              _score = 0,
+              _characteristics = [
+                {attr: 'Skill', value: 0.1, successWeight: 0.2, winInc: 0.05, loseInc: 0.02},
+                {attr: 'Energy', value: 0.5, successWeight: 0.2, winInc: 0.02, loseInc: 0.01},
+                {attr: 'Confidence', value: 0.5, successWeight: 0.2, winInc: 0.1, loseInc: -0.05},
+                {attr: 'Aggression', value: 0.5, successWeight: 0.2, winInc: 0, loseInc: 0.1},
+                {attr: 'Luck', value: 0.5, successWeight: 0.2, winInc: -0.05, loseInc: 0.05}
+              ],
+              _characteristicsCopy = _characteristics.slice();
+
+
+          function _updateCharacteristics(hasWon) {
+            var incPropertyName = hasWon === true ? 'winInc' : 'loseInc';
+
+            for (var i = 0; i < _characteristics.length; i++) {
+              var attr = _characteristics[i];
+              attr.value += attr[incPropertyName];
+              attr.value = +parseFloat(Math.min(1, Math.max(0, attr.value))).toFixed(2);
+            }
+
+            _copyCharacteristics();
+          }
+
+          function _copyCharacteristics() {
+            _characteristicsCopy = _characteristics.slice();
+          }
+
+          _visPlayer.won = function() {
+            _updateCharacteristics(true);
+
+            return _visPlayer;
+          };
+
+          _visPlayer.lost = function() {
+            _updateCharacteristics(false);
+
+            return _visPlayer;
+          };
+
+          _visPlayer.willTaunt = function() {
+            var tauntProbability =  +parseFloat(Math.random()).toFixed(2);
+            console.log(this.name() + ' taunt prob: ', tauntProbability, tauntProbability > (1 - _characteristics[CHARACTERISTICS.AGGRESSION].value));
+            return tauntProbability >= (1 - _characteristics[CHARACTERISTICS.AGGRESSION].value);
+          };
+
+          _visPlayer.characteristics = function() {
+            return _characteristicsCopy;
+          };
+
+          _visPlayer.beingTaunted = function() {
+            var randomAffectedAttributeIndex = Math.round(Math.random() * 3) + 1; // Do not include Skill
+            var positiveOrNegativeInfluenceProperty = (Math.random() > 0.5) ? 'winInc' : 'loseInc';
+
+            var attr = _characteristics[randomAffectedAttributeIndex];
+            console.log(attr, attr[positiveOrNegativeInfluenceProperty], positiveOrNegativeInfluenceProperty);
+
+            attr.value += +parseFloat(attr[positiveOrNegativeInfluenceProperty]).toFixed(2);
             attr.value = Math.min(1, Math.max(0, attr.value));
-          }
 
-          _copyCharacteristics();
+            _copyCharacteristics();
+
+            return _visPlayer;
+          };
+
+          _visPlayer.name = function(name) {
+
+            if (typeof score === 'string') {
+              _name = name;
+            }
+
+            return _name;
+          };
+
+          _visPlayer.score = function(score) {
+
+            if (typeof score === 'number') {
+              _score = score;
+            }
+
+            return _score;
+          };
+
         }
-
-        function _copyCharacteristics() {
-          _characteristicsCopy = _characteristics.slice();
-        }
-
-        _visPlayer.won = function() {
-          _updateCharacteristics(true);
-
-          return _visPlayer;
-        };
-
-        _visPlayer.lost = function() {
-          _updateCharacteristics(false);
-
-          return _visPlayer;
-        };
-
-        _visPlayer.willTaunt = function() {
-          var tauntProbability =  parseFloat(Math.random()).toFixed(2);
-          console.log(this.name() + ' taunt prob: ', tauntProbability, tauntProbability > (1 - _characteristics[CHARACTERISTICS.AGGRESSION].value));
-          return tauntProbability >= (1 - _characteristics[CHARACTERISTICS.AGGRESSION].value);
-        };
-
-        _visPlayer.characteristics = function() {
-          return _characteristicsCopy;
-        };
-
-        _visPlayer.beingTaunted = function() {
-          var randomAffectedAttributeIndex = Math.round(Math.random() * 3) + 1; // Do not include Skill
-          var positiveOrNegativeInfluenceProperty = Math.random() > 0.5 ? 'winInc' : 'loseInc';
-
-          var attr = _characteristics[randomAffectedAttributeIndex];
-
-          attr.value += parseFloat(attr[positiveOrNegativeInfluenceProperty]).toFixed(2);
-          attr.value = Math.min(1, Math.max(0, attr.value));
-
-          _copyCharacteristics();
-
-          return _visPlayer;
-        };
-
-        _visPlayer.name = function(name) {
-
-          if (typeof score === 'string') {
-            _name = name;
-          }
-
-          return _name;
-        };
-
-        _visPlayer.score = function(score) {
-
-          if (typeof score === 'number') {
-            _score = score;
-          }
-
-          return _score;
-        };
-
-      }
+      });
 })();
---
+++ app/scripts/app/directives/character-stats-bar-chart-directive.js
@@ -0,0 +1,185 @@
+(function () {
+  'use strict';
+
+  angular.module('app')
+      .directive('characterStatsBarChart', function () {
+        return {
+          restrict: 'EA',
+          scope: {
+            data: '='
+          },
+          replace: true,
+          templateUrl: 'partials/directives/character-stats-bar-chart-directive.html',
+          link: function (scope, $element) {
+
+            function Barchart(domContainer, data, width, height, padding) {
+              var _barchart = this;
+              _barchart._data = data || [];
+              _barchart._width = width || ($element.width());
+              _barchart._height = height || ($element.height() - 60);
+              _barchart._padding = padding || 0;
+
+              _barchart._containerSelection = d3.select(domContainer);
+              _barchart._svg = _barchart._containerSelection.append('svg')
+                  .classed('barchart', true)
+                  .attr('width', _barchart._width)
+                  .attr('height', _barchart._height);
+
+              _barchart._svg.append('defs')
+                  .append('clipPath')
+                  .attr('clipPathUnits', 'userSpaceOnUse')
+                  .attr('id', 'chartArea')
+                  .append('rect')
+                  .attr('fill', 'none')
+                  .attr('x', _barchart._padding / 2)
+                  .attr('y', _barchart._padding - 5)
+                  .attr('width', _barchart._width)
+                  .attr('height', _barchart._height - _barchart._padding);
+
+              // Create scale functions
+              _barchart._yScale = d3.scale.linear()
+                  .domain([1, 0])
+                  .range([20, _barchart._height - _barchart._padding]);
+
+              _barchart._xScale = d3.scale.ordinal()
+                  .domain(d3.range(_barchart._data.length))
+                  .rangeRoundBands([40, _barchart._width - 30], 0.1);
+
+
+              // Define Y axis
+              _barchart._yAxis = d3.svg.axis()
+                  .scale(_barchart._yScale)
+                  .orient('left')
+                  .ticks(5);
+
+              // Create Y axis
+              _barchart._svg.append('g')
+                  .attr('class', 'axis')
+                  .attr('transform', 'translate(40, -5)')
+                  .call(_barchart._yAxis);
+
+              _barchart._g = _barchart._svg.append('g')
+                  .attr('clip-path', 'url(#chartArea)')
+                  .attr('id', 'chart');
+
+              _barchart._colourFn = d3.scale.category10()
+                  .domain([0, _barchart._data.length])
+
+
+            }
+
+            Barchart.prototype.render = function () {
+              var _barchart = this;
+
+              var colourInterpolator = _barchart._colourFn;
+
+              var groups = _barchart._g.selectAll('g.bar')
+                  .data(_barchart._data);
+
+              groups.enter().append('g')
+                  .classed('bar', true)
+                  .attr('transform', function (d, i) {
+                    d._index = i;
+                    return 'translate(' + _barchart._xScale(i) /* move away from y-axis labels */
+                        + ', ' + (_barchart._height + _barchart._padding) + ')';
+                  });
+
+              var bars = groups.selectAll('rect.bar')
+                  .data(function (d) {
+                    return [d];
+                  });
+
+              bars.enter()
+                  .append('rect')
+                  .classed('bar', true)
+                  .attr('x', 0)
+                  .attr('y', 0)
+                  .attr('width', _barchart._xScale.rangeBand())
+                  .style('fill', function (d) {
+                    return colourInterpolator(d._index);
+                  });
+
+              bars.style('fill', function (d) {
+                    return  d3.rgb(colourInterpolator(d._index)).darker(2.5);
+                  })
+                  .transition()
+                  .delay(500)
+                  .style('fill', function (d) {
+                    return colourInterpolator(d._index);
+                  })
+                  .attr('height', function (d) {
+                return _barchart._yScale(d.value);
+              });
+
+
+              groups.transition()
+                  .delay(function (d, i) {
+                    return i / _barchart._data.length * 1000;
+                  })
+                  .attr('transform', function (d, i) {
+                    return 'translate(' + _barchart._xScale(i) + ', ' + (_barchart._height - _barchart._yScale(d.value) + 10) + ')';
+                  });
+
+              var labels = groups.selectAll('text')
+                  .data(function (d) {
+                    return [d]
+                  });
+
+              labels.enter().append('text')
+                  .attr('y', 15)
+                  .attr('fill', '#ffffff');
+
+              labels.text(function (d) {
+                return parseFloat(d.value).toFixed(2);
+              })
+              .attr('x', function() { return (_barchart._xScale.rangeBand() - this.getComputedTextLength()) / 2; })
+
+              bars.exit().remove();
+              labels.exit().remove();
+              groups.exit().remove();
+
+              return _barchart;
+
+            };
+
+            Barchart.prototype.data = function (data) {
+              var _barchart = this;
+
+              if (arguments.length === 1 && angular.isArray(data)) {
+                _barchart._data = data;
+                _barchart._yScale.domain([0, 1]);
+              }
+
+              return _barchart._data;
+            };
+
+
+            function _init(el) {
+
+
+              _barChart = new Barchart(el, scope.data).render();
+
+
+              scope.$watch(function () {
+                return scope.data;
+              }, function (data) {
+
+                if (!angular.isArray(data)) {
+                  return;
+                }
+
+                _barChart.data(data);
+                _barChart.render();
+              });
+
+            }
+
+
+            var _barChart = null;
+
+            _init($element[0]);
+
+          }
+        };
+      });
+})();
